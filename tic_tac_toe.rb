490bedbb19e83c4b2937a2c4f97ec5acb889899e
urzp <urzp002@mail.ru>
2016-06-12 22:40:43 +0300

    create check_win method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,5 @@
+LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
 class Game_place
   def initialize()
     @board = Array.new(10, " ")
@@ -16,7 +18,10 @@ class Game_place
 	@board[place]=x_o
   end
   
-  def check_win
+  def check_win(player)
+	LINES.any? do |line|
+	  line.all { |i| i == player.marker}
+	end
   end
   
   def check_full_board
