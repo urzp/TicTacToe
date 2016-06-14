0f7c4bde654399e59855c66682605341cfd92ca0
urzp <urzp002@mail.ru>
2016-06-14 17:42:04 +0300

    finish

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -192,6 +192,23 @@ end
 
 
 
+include TicTacToe
+
+game=Game_place.new
+a_man=Human.new
+comp=Computer.new
+
+puts "********************************"
+coin_toss(a_man, comp, game)
+game.draw_board
+puts "********************************"
+
+while !game.stop do
+	current_player = game.who_turn?
+	player_chose = current_player.turn(game.board)
+	game.turn(player_chose, current_player)
+end
+
 
 # ------ Nots ------
 #  is needed: 
