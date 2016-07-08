fa0f4aa0466416076063879a57b5560a892085b7
urzp <urzp002@mail.ru>
2016-07-08 11:37:04 +0300

    test wthis double

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -9,11 +9,28 @@ describe Game_place do
       a_game.board[2] ="X"
       a_game.board[3] ="X"
 
-      player_1 = double('player')
-      allow(player_1).to receve(:marker) {"X"}
-      expect(a_game.check_win(player_1)).to eq false
+      player_1 = Human.new
+      player_1.marker="X"
+
+      a_game.draw_board
+
+      expect(a_game.check_win(player_1)).to eq true
     end
 
+    it "should return true if @board instanse have a line of x" do
+      a_game = Game_place.new
+      a_game.board[1] ="X"
+      a_game.board[2] ="X"
+      a_game.board[3] ="X"
+
+      player_1 = double('human')
+      allow(player_1).to receive(:marker) {"X"}
+      allow(player_1).to receive(:name) {"Player"}
+
+      a_game.draw_board
+
+      expect(a_game.check_win(player_1)).to eq true
+    end
   end
 
 end
